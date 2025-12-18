c814319dfa2e2c63a73259ccde816fafd7635577
christiansaechao <62818511+christiansaechao@users.noreply.github.com>
2025-12-17 17:06:41 -0800

    week1 day 2

--- week-1/src/App.tsx
+++ week-1/src/App.tsx
@@ -1,45 +1,17 @@
 import "./App.css";
-
-import Header from "./components/header";
-import Body from "./components/body";
-import Footer from "./components/footer";
+import { RenderFruitList } from "./mapping/FruitList";
+import { RenderNumbers, CapitalizeWords } from "./mapping";
+import { RenderFromData } from "./mapping/RenderFromData";
+import { FormatPrices } from "./mapping/FormatPrices";
+import { EvensOnly } from "./filtering/EvensOnly";
+import { LongWordsOnly } from "./filtering/LongWordsOnly";
+import { AdminsOnly } from "./filtering/AdminsOnly";
+import { CartTotal } from "./reduce/CartTotal";
 
 function App() {
-  const navLinks = [
-    { link: "Store", href: "" },
-    { link: "Mac", href: "" },
-    { link: "Ipad", href: "" },
-    { link: "Iphone", href: "" },
-    { link: "Watch", href: "" },
-    { link: "Vision", href: "" },
-    { link: "AirPods", href: "" },
-    { link: "TV & Home", href: "" },
-    { link: "Entertainment", href: "" },
-    { link: "Accessories", href: "" },
-    { link: "Support", href: "" },
-  ];
-
-  const images = {
-    logo: {
-      src: "",
-      alt: "Apple Logo",
-    },
-    search: {
-      src: "",
-      alt: "Search Icon",
-    },
-    bag: {
-      src: "",
-      alt: "Bag Icon",
-    },
-  };
-
   return (
     <>
-      <div className="text-red-500">Testing</div>
-      <Header navLinks={navLinks} images={images} />
-      <Body />
-      <Footer />
+      <CartTotal />
     </>
   );
 }
