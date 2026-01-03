b5a2881ac8f3be35b8895c9bfb3ee6c8534770c3
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-03 11:43:11 -0500

    Update Stats solution

--- week-1/src/reduce/Stats.tsx
+++ week-1/src/reduce/Stats.tsx
@@ -1,17 +1,18 @@
 import ProblemCard from '../components/ProblemCard';
 
+
 export const Stats = () => {
   const nums = [4, 9, 2, 10, 7];
 
-  const stats = nums.reduce((acc, num) => {
-     console.log(acc, num)
-     acc.total += num;
-     acc.max = Math.max(acc.max, num);
-     acc.average = acc.total / nums.length;
+  // const stats = nums.reduce((acc, num) => {
+  //    console.log(acc, num)
+  //    acc.total += num;
+  //    acc.max = Math.max(acc.max, num);
+  //    acc.average = acc.total / nums.length;
 
-     return acc;
+  //    return acc;
 
-  }, {total: 0, max: nums[0], average: 0});
+  // }, {total: 0, max: nums[0], average: 0});
 
   return (
     <ProblemCard
@@ -26,9 +27,31 @@ export const Stats = () => {
             - max via reduce
             - average = total / nums.length
         */}
-        <div>Total: {stats.total}</div>
+        {/* <div>Total: {stats.total}</div>
         <div>Max: {stats.max}</div>
-        <div>Average: {stats.average}</div>
+        <div>Average: {stats.average}</div> */}
+
+        {Object.entries(nums.reduce((acc, num) => {
+          acc.Total += num;
+          acc.Max = Math.max(acc.Max, num);
+          acc.Average = acc.Total / nums.length;
+
+          console.log(acc);
+          console.log("Object.entries:", Object.entries(acc));
+
+          return(
+            acc
+          )
+        }, {Total: 0, Max: nums[0], Average: 0}
+      )).map(([key, value]) => {
+        console.log(key, value)
+        return (
+          <div key={key}>
+            <div key={key}><strong>{key}:</strong> {value}</div>
+          </div>
+        )
+      })
+      }
       </div>
     </ProblemCard>
   );
