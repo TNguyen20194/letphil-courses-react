421564817fd5b7a019ed4ad2ac42cb4a553332b3
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-07 00:53:10 -0500

    Update file

--- week-2/src/App.tsx
+++ week-2/src/App.tsx
@@ -4,6 +4,7 @@ import { Counter } from "./components/Counter";
 import { ToggleTheme } from "./components/ToggleTheme";
 import { Form } from "./components/Form";
 import { Cart } from "./components/Cart/Cart";
+
 function App() {
   return (
     <>
@@ -11,7 +12,8 @@ function App() {
       {/* <StudentCard name="Tony" />
       <StudentCard name="Truong" />
       <StudentCard name="Maria" /> */}
-      <ToggleTheme/>
+      {/* <ToggleTheme/> */}
+      <Form defaultName="Christian"/>
       {/* <Example /> */}
     </>
   );
--- week-2/src/components/Form.tsx
+++ week-2/src/components/Form.tsx
@@ -10,23 +10,20 @@ import React, { useState } from "react";
  */
 
 export const Form = ({ defaultName }: { defaultName: string }) => {
-  const [name, setName] = useState(defaultName);
 
-  const updateName = (e: any) => {
-    setName(e.target.value);
-  };
+  const[name, setName] = useState(defaultName);
 
   return (
     <div>
-      <p>Hello, {name}</p>
-
-      <form>
-        <label htmlFor="Update name">Update name</label>
-        <input
-          type="text"
-          name="Update name"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
+      <h1>Hello, {name}!</h1>
+      <form className="flex flex-col items-start justify-center mt-3">
+        <label htmlFor="Update name">Update Name</label>
+        <input 
+        type="text" 
+        placeholder="Enter name here" 
+        name="Update name" 
+        onChange={(e) => setName(e.target.value )}
+        className="outline-solid p-2 mt-2"
         />
       </form>
     </div>
