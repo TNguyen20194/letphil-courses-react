158a7fcc7b4129d31ed3ed6d7589662f2b93cc19
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-14 04:15:51 -0500

    Add APICopy for practice

--- week-2/src/App.tsx
+++ week-2/src/App.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import { Mounting } from "./components/useEffect/Mounting";
 import { Example } from "./components/useEffect/Example";
 import { Api } from "./components/useEffect/Api";
+import { ApiCopy } from "./components/useEffect/ApiCopy";
 
 import "./App.css";
-import { Example, StudentCard } from "./components";
-import { Counter } from "./components/Counter";
 import { ToggleTheme } from "./components/ToggleTheme";
 import { Form } from "./components/Form";
 import { Cart } from "./components/Cart/Cart";
+import { DocumentTitle } from "./components/useEffect/DocumentTitle";
 
 function App() {
   return (
     <>
-      <Cart />
+      <ApiCopy/>
+    {/* <DocumentTitle/> */}
+      {/* <Cart /> */}
       {/* <StudentCard name="Tony" />
       <StudentCard name="Truong" />
       <StudentCard name="Maria" /> */}
---
+++ week-2/src/components/useEffect/ApiCopy.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+
+// https://randomuser.me/api/?results=10
+
+/**
+ * Goal: Fetch when dependency changes.
+    - Task: fetch from a public API on component render. Store the data in state, map over data and display users, style component
+    - Constraints:
+        - Show loading state.
+        - Show error state.
+ */
+
+interface User {
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+}
+
+type UserType = User[];
+
+export const ApiCopy = () => {
+  const API = "https://randomuser.me/api/?results=10";
+
+  const [users, setUserData] = useState<UserType>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const response = await fetch(API);
+        if (!response.ok) throw new Error("Data not found");
+
+        const data = await response.json();
+
+        console.log(data.results);
+
+        if (!data.results) {
+          setUserData([]);
+          return;
+        }
+
+        setUserData(data.results);
+
+        setLoading(false);
+      } catch (err) {
+        setError(true);
+        console.log("Error:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [API]);
+
+  if (loading) {
+    return (
+      <div className="text-blue-400 font-medium text-[20px]">
+        Loading users...
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="text-red-400 font-medium text-[20px]">Error getting user data</div>;
+  }
+
+  return (
+    <>
+      {users.map((user) => {
+        const fullName = `${user.name.first} ${user.name.last}`;
+        const gender =
+          user.gender.charAt(0).toLocaleUpperCase() + user.gender.slice(1);
+
+        return (
+          <div
+            key={fullName}
+            className="grid grid-cols-2 mb-3 px-3 py-3 bg-blue-500 rounded-[12px] text-[16px] hover:bg-blue-500/90"
+          >
+            <div>
+              <span className="font-semibold">Name:</span> {fullName}
+            </div>
+            <div>
+              <span className="font-semibold">Gender:</span> {gender}
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
